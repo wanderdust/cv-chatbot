c4e9e9af62cee6910bb08310f195326c45222c3d
Pablo Lopez <pablo.lopez.santori@gmail.com>
2023-01-02 22:52:27 +0000

    Change build platform

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ src/models/minilm-uncased-squad2/config.json:
 
 build: src/models/minilm-uncased-squad2/config.json
 	aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
-	docker build -t pablo-cv-chatbot .
+	docker build -t pablo-cv-chatbot . --platform linux/amd64
 
 authenticate:
 	aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 236212633992.dkr.ecr.eu-west-1.amazonaws.com
--- main.tf
+++ main.tf
@@ -18,8 +18,8 @@ resource "aws_lambda_function" "cv_chatbot" {
 
   package_type  = "Image"
   image_uri     = var.image_uri
-  architectures = ["arm64"]
-  timeout       = 300
+  architectures = ["x86_64"]
+  timeout       = 60
   memory_size   = 512
   environment {
     variables = {
@@ -90,67 +90,6 @@ resource "aws_iam_role" "lambda_exec" {
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-
-resource "aws_apigatewayv2_api" "lambda" {
-  name          = "serverless_lambda_gw"
-  protocol_type = "HTTP"
-}
-
-resource "aws_apigatewayv2_stage" "lambda" {
-  api_id = aws_apigatewayv2_api.lambda.id
-
-  name        = "serverless_lambda_stage"
-  auto_deploy = true
-
-  access_log_settings {
-    destination_arn = aws_cloudwatch_log_group.api_gw.arn
-
-    format = jsonencode({
-      requestId               = "$context.requestId"
-      sourceIp                = "$context.identity.sourceIp"
-      requestTime             = "$context.requestTime"
-      protocol                = "$context.protocol"
-      httpMethod              = "$context.httpMethod"
-      resourcePath            = "$context.resourcePath"
-      routeKey                = "$context.routeKey"
-      status                  = "$context.status"
-      responseLength          = "$context.responseLength"
-      integrationErrorMessage = "$context.integrationErrorMessage"
-      }
-    )
-  }
-}
-
-resource "aws_apigatewayv2_integration" "cv_chatbot" {
-  api_id = aws_apigatewayv2_api.lambda.id
-
-  integration_uri    = aws_lambda_function.cv_chatbot.invoke_arn
-  integration_type   = "AWS_PROXY"
-  integration_method = "POST"
-}
-
-resource "aws_apigatewayv2_route" "cv_chatbot" {
-  api_id = aws_apigatewayv2_api.lambda.id
-
-  route_key = "GET /chat"
-  target    = "integrations/${aws_apigatewayv2_integration.cv_chatbot.id}"
-}
-
-resource "aws_cloudwatch_log_group" "api_gw" {
-  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
-
-  retention_in_days = 5
-}
-
-resource "aws_lambda_permission" "api_gw" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.cv_chatbot.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
 }
 
 
