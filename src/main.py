97a9bd375c8279bb3d489715ee0cea88e2959670
Pablo Lopez <pablo.lopez.santori@gmail.com>
2023-01-02 23:59:34 +0000

    Get rid of fastapi

--- src/main.py
+++ src/main.py
@@ -6,12 +6,16 @@ from models import ChatBot, GreetingDetector
 chatbot = ChatBot()
 
 
+def response(response):
+    return {"statusCode": 200, "body": json.dumps(response)}
+
+
 def handler(event, context):
     message = json.loads(event["body"])["message"]
     try:
         if GreetingDetector.is_greeting(message):
-            return {"response": GreetingDetector.default_response()}
+            return response(GreetingDetector.default_response())
 
-        return {"response": chatbot.predict(message)}
+        return response(chatbot.predict(message))
     except Exception:
         raise Exception(traceback.format_exc())
