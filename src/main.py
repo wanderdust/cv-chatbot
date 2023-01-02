14a95008208c5163070dd71b7471bcb9d1d632e1
Pablo Lopez <pablo.lopez.santori@gmail.com>
2023-01-02 23:22:45 +0000

    Remove fastapi

--- src/main.py
+++ src/main.py
@@ -1,30 +1,17 @@
+import json
 import traceback
 
-from fastapi import FastAPI, HTTPException
-from mangum import Mangum
-from pydantic import BaseModel
-
 from models import ChatBot, GreetingDetector
-
-app = FastAPI()
-
-
-class ChatResponse(BaseModel):
-    response: str
-
 
 chatbot = ChatBot()
 
 
-@app.get("/chat", response_model=ChatResponse)
-def root(message: str):
+def handler(event, context):
+    message = json.loads(event["body"])["message"]
     try:
         if GreetingDetector.is_greeting(message):
             return {"response": GreetingDetector.default_response()}
 
         return {"response": chatbot.predict(message)}
     except Exception:
-        raise HTTPException(status_code=400, detail=traceback.format_exc())
-
-
-handler = Mangum(app)
+        raise Exception(traceback.format_exc())
