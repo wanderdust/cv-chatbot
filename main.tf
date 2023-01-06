f831fae3cae0611516821094d8c9fb9dea517215
Pablo Lopez <pablo.lopez.santori@gmail.com>
2023-01-06 08:53:50 +0000

    Narrow the allowed sites

--- main.tf
+++ main.tf
@@ -97,7 +97,7 @@ resource "aws_apigatewayv2_api" "lambda" {
   name          = "cv_chatbot_gw"
   protocol_type = "HTTP"
   cors_configuration {
-    allow_origins = ["*"]
+    allow_origins = ["http://localhost:3000/,https://pablolopez.tech/"]
     allow_methods = ["OPTIONS", "GET", "POST"]
     allow_headers = ["Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token"]
     max_age       = 400
